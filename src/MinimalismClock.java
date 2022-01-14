7747dffeb5c60a0d8f7c89379858dfd794143664
kke <hohehe9307@naver.com>
2022-01-14 14:35:53 +0900

    swea 문제 풀이중

--- src/MinimalismClock.java
+++ src/MinimalismClock.java
@@ -8,6 +8,7 @@ public class MinimalismClock {
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 	}
 
 }
